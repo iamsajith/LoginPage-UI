8fcb40cedafea0d64ad26d923308aabed34da747
iamsajith <sajithj03@gmail.com>
2022-02-05 21:25:45 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/rightswipe/smart_vehicle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rightswipe.smart_vehicle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import './screens/login-page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Car Health',
+      theme: ThemeData(
+        textTheme:
+            GoogleFonts.josefinSansTextTheme(Theme.of(context).textTheme),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
